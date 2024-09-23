8d28fd4180d6dae7ffe9ece1ef4e34ad4cd8b197
ReiLoko <thiagovviphone@gmail.com>
2024-09-22 22:00:59 -0300

    fix a lot of things

--- MangaYouKnowDesktop/src/components/UltraFavoriteCard.vue
+++ MangaYouKnowDesktop/src/components/UltraFavoriteCard.vue
@@ -32,7 +32,7 @@
 <template>
   <div>
     <div class="bg-gray-900 rounded-xl h-[280px] w-40 flex flex-col !p-0 items-center">
-    <UTooltip :text=favorite.name placement="bottom">
+    <UTooltip :text=favorite.name :popper="{ placement: 'bottom', arrow: true }" >
       <UBadge class="w-36 m-1 flex justify-center" color="white" variant="solid" >
         {{ favorite.name.substring(0, 16) + (favorite.name.length > 16? "..." : "") }}
       </UBadge>
@@ -46,7 +46,7 @@
         variant="solid"
         icon="i-heroicons-book-open-solid"
       />
-      <UTooltip :text="chaptersReaded" :popper="{ placement: 'top' }" >
+      <UTooltip :text="chaptersReaded" :popper="{ placement: 'top', arrow: true }" >
         <UButton
           :loading="isLoading"
           color="white"
