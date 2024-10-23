1f6495a7e1b2d1caf0d1163320c7bb5618c82685
kingtimer12 <queentimer12@gmail.com>
2024-10-23 08:14:48 -0300

    CI: Updated script and github actions

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 TOML_FILE="./src-tauri/Cargo.toml"
+TAURI_FILE="./src-tauri/tauri.conf.json"
 
 if [ -z "$1" ]; then
   echo "Use: ./update.sh 1.2.3"
@@ -18,3 +19,8 @@ current_version=$(grep -E "^version = \"[0-9]+\.[0-9]+\.[0-9]+\"" $TOML_FILE | c
 sed -i "s/version = \"$current_version\"/version = \"$new_version\"/" $TOML_FILE
 
 echo "$current_version -> $new_version ($TOML_FILE)"
+
+current_version=$(grep -E "^\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"" $TAURI_FILE | cut -d '"' -f 2)
+sed -i "s/\"version\": \"$current_version\"/\"version\": \"$new_version\"/" $TAURI_FILE
+
+echo "$current_version -> $new_version ($TAURI_FILE)"
