b29f372719a564131490cbcefd7c780cb480b27c
ReiLoko <thiagovviphone@gmail.com>
2024-11-07 22:17:21 -0300

    FIX: now the remove from mark is actually reactive.

--- src/components/SelectedDropdown.vue
+++ src/components/SelectedDropdown.vue
@@ -11,75 +11,51 @@ const isMarkSelectedModalOpen = useState<boolean>(
     "isMarkSelectedModalOpen",
     () => false,
 );
-const items = [
-    [
-        {
-            label: "Add to Mark",
-            icon: "i-heroicons-archive-box-20-solid",
-            click: () => {
-                isMarkSelectedModalOpen.value = true;
-            },
-        },
-        //     {
-        //         label: "Edit",
-        //         icon: "i-heroicons-pencil-square-20-solid",
-        //     },
-        // ],
-        // [
-        //     {
-        //         label: "Add selected to Mark",
-        //         icon: "i-heroicons-archive-box-20-solid",
-        //     },
-        //     {
-        //         label: "Ultrafavorite selected",
-        //         icon: "i-heroicons-star-20-solid",
-        //         click: async () => {
-        //             await FavoriteDB.ultraFavoriteAll(selectedFavorites.value);
-        //             isSelecting.value = false;
-        //         },
-        //     },
-    ],
-    [
-        // {
-        //     label: "Remove ultrafavorite",
-        //     icon: "i-heroicons-star",
-        // },
-        {
-            label: `Remove from ${currentlyMark.value}`,
-            icon: "i-heroicons-minus-circle-solid",
-            //    disabled: currentlyMark.value == "-",
-            click: async () => {
-                await MarkFavoriteDB.deleteMarkFavorites(
-                    selectedFavorites.value,
-                    await MarkDB.getMarkId(currentlyMark.value),
-                );
-                favorites.value = await FavoriteDB.getFavorites(user.value.id);
-                isSelecting.value = false;
-            },
-        },
-        {
-            label: "Delete selected",
-            icon: "i-heroicons-trash-20-solid",
-            click: async () => {
-                const answer = await confirm(
-                    `This will delete ${selectedFavorites.value.length} favorite(s). Are you sure?`,
-                    { title: "Delete selected", kind: "warning" },
-                );
-                if (answer) {
-                    await FavoriteDB.deleteFavorites(selectedFavorites.value);
-                    favorites.value = await FavoriteDB.getFavorites(
-                        user.value.id,
-                    );
-                    isSelecting.value = false;
-                }
-            },
-        },
-    ],
-];
 </script>
 
 <template>
-    <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+    <UDropdown
+        :items="[
+            [
+                {
+                    label: 'Add to Mark',
+                    icon: 'i-heroicons-archive-box-20-solid',
+                    click: () => (isMarkSelectedModalOpen = true),
+                },
+            ],
+            [
+                {
+                    label: `Remove from ${currentlyMark}`,
+                    icon: 'i-heroicons-minus-circle-solid',
+                    disabled: currentlyMark === '-',
+                    click: async () => {
+                        await MarkFavoriteDB.deleteMarkFavorites(
+                            selectedFavorites,
+                            await MarkDB.getMarkId(currentlyMark),
+                        );
+                        favorites = await FavoriteDB.getFavorites(user.id);
+                        isSelecting = false;
+                    },
+                },
+                {
+                    label: 'Delete selected',
+                    icon: 'i-heroicons-trash-20-solid',
+                    click: async () => {
+                        const answer = await confirm(
+                            `This will delete ${selectedFavorites.length} favorite(s). Are you sure?`,
+                            { title: 'Delete selected', kind: 'warning' },
+                        );
+                        if (answer) {
+                            await FavoriteDB.deleteFavorites(selectedFavorites);
+                            favorites = await FavoriteDB.getFavorites(user.id);
+                            isSelecting = false;
+                        }
+                    },
+                },
+            ],
+        ]"
+        :popper="{ placement: 'bottom-start' }"
+    >
         <UButton
             color="white"
             :label="isSelecting ? selectedFavorites.length.toString() : ''"
