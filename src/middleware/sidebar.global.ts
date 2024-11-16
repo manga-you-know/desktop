67a865d1f5ed9f77e0e82f35c3babac38c0e6014
ReiLoko <thiagovviphone@gmail.com>
2024-11-15 22:56:51 -0300

    FEAT: now you can disable auto enter fullscreen

--- src/middleware/sidebar.global.ts
+++ src/middleware/sidebar.global.ts
@@ -1,13 +1,17 @@
-import { getCurrentWindow } from '@tauri-apps/api/window';
-export default defineNuxtRouteMiddleware((to, from) => {
-	const window = getCurrentWindow();
-	const activeSidebar = useState<boolean>('activeSidebar');
-	const isFavoriteOpen = useState<boolean>('isFavoriteOpen');
-	const isSelecting = useState<boolean>('isSelecting')
-	isSelecting.value = false;
-	isFavoriteOpen.value = false;
-	activeSidebar.value = !to.path.startsWith('/reader');
-	window.setFullscreen(to.path.startsWith('/reader'));
-	if (to.path === '/reader') return navigateTo('/')
-	if (!to.path.startsWith('/reader')) window.setTitle(`MangaYouKnow - ${to.meta.name}`)
-})
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { load } from "@tauri-apps/plugin-store";
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const window = getCurrentWindow();
+  const config = await load("config.json");
+  const activeSidebar = useState<boolean>("activeSidebar");
+  const isFavoriteOpen = useState<boolean>("isFavoriteOpen");
+  const isSelecting = useState<boolean>("isSelecting");
+  isSelecting.value = false;
+  isFavoriteOpen.value = false;
+  activeSidebar.value = !to.path.startsWith("/reader");
+  const autoEnter = await config.get<boolean>("auto_enter_fullscreen");
+  window.setFullscreen((to.path.startsWith("/reader") && autoEnter) || false);
+  if (to.path === "/reader") return navigateTo("/");
+  if (!to.path.startsWith("/reader"))
+    window.setTitle(`MangaYouKnow - ${to.meta.name}`);
+});
--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -6,6 +6,7 @@ definePageMeta({
     name: "Settings",
 });
 const autoSearchUpdates = ref(true);
+const autoEnterFullscreen = ref(true);
 const theme = ref("dark");
 const favoriteLanguage = ref("en");
 
@@ -21,15 +22,21 @@ async function searchUpdate() {
     isSearchingUpdate.value = false;
 }
 onBeforeMount(async () => {
-    const [autoSearchSaved, themeSaved, favoriteLanguageSaved, appVersion] =
-        await Promise.all([
-            config.get<boolean>("auto_search_updates"),
-            config.get<string>("theme"),
-            config.get<string>("favorite_language"),
-            getVersion(),
-        ]);
-    console.log(autoSearchSaved);
+    const [
+        autoSearchSaved,
+        autoEnterFullSaved,
+        themeSaved,
+        favoriteLanguageSaved,
+        appVersion,
+    ] = await Promise.all([
+        config.get<boolean>("auto_search_updates"),
+        config.get<boolean>("auto_enter_fullscreen"),
+        config.get<string>("theme"),
+        config.get<string>("favorite_language"),
+        getVersion(),
+    ]);
     autoSearchUpdates.value = autoSearchSaved ?? true;
+    autoEnterFullscreen.value = autoEnterFullSaved ?? true;
     theme.value = themeSaved ?? "dark";
     favoriteLanguage.value = favoriteLanguageSaved ?? "en";
     currentlyVersion.value = appVersion;
@@ -37,7 +44,7 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-    <div class="w-full h-screen flex flex-col pt-8 gap-3">
+    <div class="w-full h-screen flex flex-col pt-8 pr-4 gap-3">
         <UDivider label="Version" size="xl" />
         <div class="pl-3">
             <UAlert :title="`Current version: v${currentlyVersion}`">
@@ -63,5 +70,19 @@ onBeforeMount(async () => {
                 </template>
             </UAlert>
         </div>
+        <UDivider label="Reader" size="xl" />
+        <UAlert>
+            <template #description>
+                <UCheckbox
+                    class="my-3"
+                    label="Auto enter in fullscreen"
+                    color="cyan"
+                    v-model:model-value="autoEnterFullscreen"
+                    v-on:update:model-value="
+                        saveConfig('auto_enter_fullscreen', autoEnterFullscreen)
+                    "
+                />
+            </template>
+        </UAlert>
     </div>
 </template>
