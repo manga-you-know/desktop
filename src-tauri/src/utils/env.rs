0905310c9f5d35ec86e9e4bf96376a0e9d03e4f2
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2025-05-07 20:48:51 -0300

    FIX: THE WORST ERROR IS OVER!!! some intense bug was happening.

--- src-tauri/src/utils/env.rs
+++
@@ -1,4 +0,0 @@
-#[tauri::command]
-pub fn get_env(name: &str) -> String {
-    std::env::var(String::from(name)).unwrap_or(String::from(""))
-}
--- src-tauri/src/utils/mod.rs
+++ src-tauri/src/utils/mod.rs
@@ -1,3 +1,2 @@
 pub mod hashmap;
 pub mod request;
-pub mod env;
