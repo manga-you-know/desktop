2db72d79efe955c7aae4fb2524d2a937b83186e6
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2025-05-25 22:07:01 -0300

    FIX: default config for plugin cache

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,15 @@ mod utils;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let cache_config = tauri_plugin_cache::CacheConfig {
+        cache_dir: Some("cache".into()),                                  
+        cache_file_name: Some("cache_data.json".into()),                        
+        cleanup_interval: Some(120),                                             
+        default_compression: Some(true),                                         
+        compression_level: Some(7),                                              
+        compression_threshold: Some(4096),                                       
+        compression_method: Some(tauri_plugin_cache::CompressionMethod::Lzma2)
+    };
     #[allow(unused_mut)]
     let mut builder = tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
@@ -17,41 +26,41 @@ pub fn run() {
             get_base64_image
         ])
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_cache::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_sql::Builder::new().build())
-        .plugin(tauri_plugin_http::init());
-        #[cfg(desktop)]
-        {
-            builder = builder
-            .setup(|app| {
-                let window = app.get_webview_window("main").unwrap();
-                let args: Vec<String> = std::env::args().collect();
-                if args.contains(&"--flag1".to_string()) {
-                    window.hide().unwrap();
-                }
-                Ok(())
-            })
-            .plugin(tauri_plugin_clipboard::init())
-            .plugin(tauri_plugin_drpc::init())
-            .plugin(tauri_plugin_cli::init())
-            .plugin(tauri_plugin_window_state::Builder::default().build())
-            .plugin(tauri_plugin_updater::Builder::new().build())
-            .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
-                let win = app.get_webview_window("main").unwrap();
-                win.show().unwrap();
-                win.set_focus().unwrap();
-            }))
-            .plugin(tauri_plugin_autostart::init(
-                tauri_plugin_autostart::MacosLauncher::LaunchAgent,
-                Some(vec!["--flag1", "--flag2"]),
-            ))
-            .plugin(tauri_plugin_global_shortcut::Builder::new().build());
+        .plugin(tauri_plugin_cache::init_with_config(cache_config));
+    #[cfg(desktop)]
+    {
+        builder = builder
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            let args: Vec<String> = std::env::args().collect();
+            if args.contains(&"--flag1".to_string()) {
+                window.hide().unwrap();
+            }
+            Ok(())
+        })
+        .plugin(tauri_plugin_clipboard::init())
+        .plugin(tauri_plugin_drpc::init())
+        .plugin(tauri_plugin_cli::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let win = app.get_webview_window("main").unwrap();
+            win.show().unwrap();
+            win.set_focus().unwrap();
+        }))
+        .plugin(tauri_plugin_autostart::init(
+            tauri_plugin_autostart::MacosLauncher::LaunchAgent,
+            Some(vec!["--flag1", "--flag2"]),
+        ))
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build());
     }
     builder.run(tauri::generate_context!()).unwrap()
 }
